ef57fdba9dd6e5591c05d984aaae59b479419b7e
Minho Ryang <minhoryang@gmail.com>
2016-03-11 23:49:05 +0900

    tcap.py, now CUI supported, and [WIP] playback.

--- tcap.py
+++ tcap.py
@@ -2,13 +2,24 @@ from sys import (
     stdin as _stdin,
     stdout as _stdout,
 )
-import json
 from select import select
+from json import (
+    dump as json_dump,
+    load as json_load,
+)
+
+from click import (
+    argument,
+    command,
+    option,
+    Choice,
+    Path,
+)
 
 from process_capsule import PythonCapsule as PC
 
 
-def stdout(stdout):
+def _feedback_to_user(stdout):
     """."""
     if isinstance(stdout, (bytes)):
         _stdout.write(stdout.decode("utf-8"))
@@ -16,26 +27,70 @@ def stdout(stdout):
         _stdout.write(str(stdout))
     _stdout.flush()
 
-def stdin(log=None):
+
+def _in_out_hook(terminal, log=None):
     """try to read from user, send it to program."""
     # try to read stdin
     stdin, _, _ = select([_stdin], [], [], 0)
     if stdin:
         stdin = stdin[0].readline().rstrip()
-        stdout = a.write(stdin)[1]
+        stdout = terminal.write(stdin)[1]
         if log is not None:
             log.append((stdin, stdout))
     else:
-        stdout = a.read()
+        stdout = terminal.read()
         if stdout != b'' and log is not None:
             log.append((None, stdout))
     return stdout
 
 
-a = PC("test5.py")
-a.run(with_check=False)
-b = []
-while not a.is_dead():
-    stdout(stdin(b))
-from pprint import pprint
-pprint(b)
+def _in_out_stream(terminal, stdin, stdout):
+    if stdin:
+        terminal.write(stdin, response=False)
+    return terminal.read(timeout=1)
+
+
+@command()
+@argument('program', type=Path())
+@option('-j', '--json', type=Path(), default=None) 
+@option('--mode', 'mode', type=Choice([
+    'capture',
+    'playback',
+    'validate',
+]), default='capture')
+def terminal(program, json=None, mode=False):
+    if mode == 'capture':
+        terminal_capture(program, json)
+    elif mode == 'playback':
+        terminal_playback(program, json)
+    elif mode == 'validate':
+        pass  # TODO
+
+
+def terminal_capture(program, json=None):
+    captured = []
+    with PC(program) as terminal:
+        terminal.run(with_check=False)
+        while not terminal.is_dead():
+            _feedback_to_user(_in_out_hook(terminal, captured))
+    if json:
+        with open(json, 'w') as fp:
+            json_dump(captured, fp)
+    return captured
+
+
+def terminal_playback(program, json):
+    if json is None:
+        raise Exception("-j, --json needed!")
+    with open(json, 'r') as fp:
+        captured = json_load(fp)
+        with PC(program, logfile=open("test.log", "wb")) as terminal:
+            terminal.run(with_check=False)
+            for stdin, stdout in captured:
+                # TODO : NOT YET TO WORK!
+                _feedback_to_user(_in_out_stream(terminal, stdin, None))
+            _feedback_to_user(_in_out_stream(terminal, None, None))
+
+
+if __name__ == "__main__":
+    terminal()
